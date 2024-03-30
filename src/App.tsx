e075ce9174744a8e0bdb602c6b9a083fde8eb7ac
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2024-03-30 21:25:17 +0100

    Add ESLint and husky. Add scss and create base folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
+import { Header } from './components/layout/Header/Header';
 
-import logo from './logo.svg';
-import { TestInterface } from 'types';
-
-import './App.css';
+import './App.scss';
 
 function App() {
-  const foo: TestInterface = {
-    x: 4,
-  };
-
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {foo.x}
-        </a>
+        <Header />
       </header>
     </div>
   );
 }
 
-export default App;
+export { App };
---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return <p className={styles.header}>WELCOME TO TO DO ZENN APP</p>;
+};
+
+export { Header };
