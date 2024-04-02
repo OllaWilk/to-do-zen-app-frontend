cc62766dae9764c16ea597029e1af1513298c743
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2024-04-02 13:48:04 +0200

    add bassic documentation

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import parse from 'html-react-parser';
+import { pageContents } from '../../../data/dataStore';
 import styles from './Header.module.scss';
 
 const Header = () => {
-  return <p className={styles.header}>WELCOME TO TO DO ZENN APP</p>;
+  return (
+    <>
+      <h1 className={styles.header}>{parse(pageContents.title)}</h1>
+      <p>{pageContents.subtitle}</p>
+    </>
+  );
 };
 
 export { Header };
---
+++ src/data/dataStore.ts
@@ -0,0 +1,9 @@
+interface PageContents {
+  title: string;
+  subtitle: string;
+}
+
+export const pageContents: PageContents = {
+  title: 'Things to do zen <sup>soon!</sup>',
+  subtitle: 'A simple to-do and tracker app, with lists, columns and cards',
+};
