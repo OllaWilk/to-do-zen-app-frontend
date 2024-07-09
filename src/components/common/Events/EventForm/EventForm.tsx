6702605fb6c3115fe1a104f5ea5cc130d3115a88
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2024-07-09 18:28:44 +0200

    Add address input and conversion to coordinates in EventForm
    - Added the ability to input an address in the EventForm.
    - Implemented functionality to convert the entered address to longitude and latitude coordinates.
    - Included error handling for cases where the address cannot be found.
    - Updated form submission to use the fetched coordinates.

--- src/components/common/Events/EventForm/EventForm.tsx
+++ src/components/common/Events/EventForm/EventForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { EventEntity, NewEventEntity, EventStatus } from 'types';
 import { formatDate, useEventFetch } from '../../../../utils/hooks';
 import { Form, Input, Textarea, Select } from '../../Form';
@@ -8,10 +8,19 @@ interface Props {
   event?: EventEntity;
 }
 
-export const EventForm = ({ event }: Props) => {
-  const { eventInsert, error } = useEventFetch();
-  const eventDataFormat = formatDate(`${event?.event_date}`);
+interface NewEventEntityWithAdress extends NewEventEntity {
+  address?: string;
+}
 
+export const EventForm = ({ event }: Props) => {
+  // Hook to handle event insertion and error handling
+  const { eventInsert, error } = useEventFetch();
+  // Format the event date for display in the form
+  const eventDataFormat = formatDate(`${event?.event_date}`);
+  // State to manage location error messages
+  const [errorLocation, setErrorLocation] = useState<string | null>(null);
+
+  // Initial form values, using event data if available
   const formValues = {
     title: event?.title || '',
     status: event?.status || ('planed' as EventStatus),
@@ -22,19 +31,49 @@ export const EventForm = ({ event }: Props) => {
     event_date: event?.event_date ? (eventDataFormat as Date) : null,
     reminder: event?.reminder || 1,
     creator_id: event?.creator_id || '',
-    lat: 51.107883,
-    lon: 17.038538,
+    lat: event?.lat,
+    lon: event?.lon,
   };
 
   /* SUBMIT FORM */
-  const submit = async (form: NewEventEntity) => {
-    if (event) {
-      await eventInsert({
-        ...event,
-        ...form,
-      });
-    } else {
-      await eventInsert(form);
+  const submit = async (form: NewEventEntityWithAdress) => {
+    // Construct the Nominatim API URL for location search
+    const locationSearch = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+      `${form.address}`
+    )}`;
+
+    try {
+      // Fetch the location data from Nominatim API
+      const response = await fetch(locationSearch);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      // Parse the response JSON to get the location data
+      const location = await response.json();
+
+      // Set the latitude and longitude from the location data
+      form.lon = parseFloat(location[0].lon);
+      form.lat = parseFloat(location[0].lat);
+
+      // Insert or update the event with the form data
+      if (event) {
+        await eventInsert({
+          ...event,
+          ...form,
+        });
+      } else {
+        await eventInsert(form);
+      }
+
+      // Clear any previous location error messages
+      setErrorLocation(null);
+    } catch (error) {
+      console.error(error);
+      // Set an error message if location fetch fails
+      setErrorLocation(
+        'Sorry, no location found. Please check for typos and ensure the format is City, Street Name, House Number. For example: "Wroclaw, Pereca 1". Avoid using abbreviations like "ul.", "al.", etc.'
+      );
     }
   };
 
@@ -54,6 +93,7 @@ export const EventForm = ({ event }: Props) => {
           placeholder={'title'}
         />
         <Input label={'Date'} name='event_date' type='date' />
+        <Input label={'address'} name='address' />
         <Select
           label={'status'}
           name='status'
@@ -69,12 +109,10 @@ export const EventForm = ({ event }: Props) => {
           minLength={3}
           maxHeight={100}
         />
-        <Input
-          label={'Remind me (days before event)'}
-          name='reminder'
-          type='number'
-        />
         <p className={`${error ? styles.error : styles.success}`}>{error}</p>
+        <p className={`${errorLocation ? styles.error : styles.success}`}>
+          {errorLocation}
+        </p>
       </Form>
     </div>
   );
